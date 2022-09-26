ac06b31cdf1511bc7acd57a3bebe73f0b869b62d
yongckim <zxcv9203@naver.com>
2022-09-26 21:30:05 +0900

    feat : 1시간이 지날 경우 벨소리가 들리는 기능 추가
    go-mp3 라이브러리를 사용하여 cli 환경에서 mp3 파일 실행기능 구현

--- cmd/algorim/main.go
+++ cmd/algorim/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/hajimehoshi/go-mp3"
+	"github.com/hajimehoshi/oto/v2"
+)
 
 func main() {
-	fmt.Println("excute Algorim")
+	fmt.Println("Algorim이 실행중입니다.")
+	timer := time.NewTimer(time.Hour * 1)
+	<-timer.C
+	fmt.Println("Algorim이 종료되었습니다.")
+	f, err := os.Open("./assets/end.mp3")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	d, err := mp3.NewDecoder(f)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	c, ready, err := oto.NewContext(d.SampleRate(), 2, 2)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	<-ready
+
+	p := c.NewPlayer(d)
+	defer func(p oto.Player) {
+		err := p.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}(p)
+	p.Play()
+
+	for {
+		time.Sleep(time.Second)
+		if !p.IsPlaying() {
+			break
+		}
+	}
 }
+
--- go.mod
+++ go.mod
@@ -1,1 +1,10 @@
 module github.com/zxcv9203/algorim
+
+go 1.19
+
+require (
+	github.com/hajimehoshi/go-mp3 v0.3.3
+	github.com/hajimehoshi/oto/v2 v2.3.1
+)
+
+require golang.org/x/sys v0.0.0-20220712014510-0a85c31ab51e // indirect
